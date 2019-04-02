88db872162b68d4707b373c0cc340483912ff215
Junwon Jung <junwon3329@gmail.com>
2019-04-02 13:55:56 -0500

    Committing merge two sorted lists

---
+++ Top Interview Questions/21. Merge Two Sorted Lists/solution.py
@@ -0,0 +1,35 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if not l1 or not l2:
+            return l1 if l1 else l2
+        head = None
+        if l1.val < l2.val:
+            head = l1
+            l1 = l1.next
+        else:
+            head = l2
+            l2 = l2.next
+        cur = head
+        while l1 and l2:
+            if l1.val < l2.val:
+                cur.next = l1
+                l1 = l1.next
+            else:
+                cur.next = l2
+                l2 = l2.next
+            cur = cur.next
+        while l1:
+            cur.next = l1
+            l1 = l1.next
+            cur = cur.next
+        while l2:
+            cur.next = l2
+            l2 = l2.next
+            cur = cur.next
+        return head
