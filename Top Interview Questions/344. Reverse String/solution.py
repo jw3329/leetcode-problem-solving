f7b1ea965c2aecaa6f92f31adf591bd8313421fb
Junwon Jung <junwon3329@gmail.com>
2019-07-25 11:22:51 -0500

    Committing reverse string problem

---
+++ Top Interview Questions/344. Reverse String/solution.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: List[str]
+        :rtype: None Do not return anything, modify s in-place instead.
+        """
+        start, end = 0, len(s) - 1
+        
+        while start < end:
+            temp = s[start]
+            s[start] = s[end]
+            s[end] = temp
+            start += 1
+            end -= 1
