5356b1a0dce41b54b3b9016efcbced353abaddb6
Junwon Jung <junwon3329@gmail.com>
2022-07-29 15:08:25 -0500

    feat: candy problem commit

---
+++ review-lists/135. Candy/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        nums = [1] * len(ratings)
+        for i in range(1, len(ratings)):
+            if ratings[i] > ratings[i-1]:
+                nums[i] = nums[i-1] + 1
+        for i in range(len(ratings) - 1, 0, -1):
+            if ratings[i-1] > ratings[i]:
+                nums[i-1] = max(nums[i-1], nums[i] + 1)
+        return sum(nums)
