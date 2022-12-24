5bafc90f787ade5ff11a476326826f0182f6a0a6
Junwon Jung <junwon3329@gmail.com>
2022-12-24 02:46:12 -0600

    feat: binary tree level order commit

---
+++ 107. Binary Tree Level Order Traversal II/solution.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrderBottom(self, root: Optional[TreeNode]) -> List[List[int]]:
+        # reverse after bfs
+        if not root: return []
+        queue = deque([root])
+        res = []
+        while queue:
+            temp = []
+            for _ in range(len(queue)):
+                curr = queue.popleft()
+                temp.append(curr.val)
+                if curr.left:
+                    queue.append(curr.left)
+                if curr.right:
+                    queue.append(curr.right)
+            res.append(temp)
+        return reversed(res)
